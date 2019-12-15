e45c3622cda0b2d039c68b695c55745e8df87fb1
Joseph S. Cha <josephshincha@gmail.com>
2019-12-15 00:55:36 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,14 @@
+# def current_age_for_birth_year(x)
+#   age_of_person = (2019 - x)
+#   puts age_of_person
+# end
+
+# current_age_for_birth_year(1984)
+
+#Current year 2019. 1984 returns 35
 def current_age_for_birth_year(x)
-  age_of_person = (2019 - x)
-  puts age_of_person
+  y = (2019 - x)
+  age_of_person = (-16 + y)
 end
 
 current_age_for_birth_year(1984)
-
