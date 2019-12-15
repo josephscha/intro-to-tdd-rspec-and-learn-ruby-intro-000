4bb841cfbd09a62cadc97f5a09a0013d7029e6b5
Joseph S. Cha <josephshincha@gmail.com>
2019-12-15 00:50:23 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
-def current_age_for_birth_year(x)
-  age_of_person = (2019 - x)
-  puts age_of_person
-end
+# def current_age_for_birth_year(x)
+#   age_of_person = (2019 - x)
+#   puts age_of_person
+# end
 
-current_age_for_birth_year(2000)
+# current_age_for_birth_year(2000)
