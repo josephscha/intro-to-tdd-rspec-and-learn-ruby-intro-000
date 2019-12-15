c9783b1250b1c4d722bfedc371f214119f6e2eea
Joseph S. Cha <josephshincha@gmail.com>
2019-12-15 00:59:44 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -6,7 +6,7 @@
 # current_age_for_birth_year(1984)
 
 def current_age_for_birth_year(birth_year) 
-  age_of_person = 2000 - birth_year
+  age_of_person = 2003 - birth_year
 end
 
 current_age_for_birth_year(1984)
